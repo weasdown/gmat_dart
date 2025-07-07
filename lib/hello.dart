c62fbb5e90b452fdca406e6403ab51288e1e816a
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-07 14:51:00 +0100

    refactor: use PlatformOption to improve readability, avoid unnecessary ifs

--- lib/hello.dart
+++ lib/hello.dart
@@ -7,6 +7,8 @@ import 'dart:io' show Platform, Directory;
 
 import 'package:path/path.dart' as path;
 
+import 'platform_option.dart';
+
 final String helloLibraryPath = 'C_libraries\\hello_library\\build';
 
 // FFI signature of the hello_world C function
@@ -16,28 +18,28 @@ typedef HelloWorld = void Function();
 
 void main() {
   // Open the dynamic library
-  var libraryPath = path.join(
-    Directory.current.path,
-    helloLibraryPath,
-    'libhello.so',
-  );
-
-  if (Platform.isMacOS) {
-    libraryPath = path.join(
+  // TODO: refactor out path.join by using a List.generate that switches across PlatformOptions.
+  final String libraryPath = switch (PlatformOption.current) {
+    PlatformOption.linux => path.join(
+      Directory.current.path,
+      helloLibraryPath,
+      'libhello.so',
+    ),
+    PlatformOption.macOS => path.join(
       Directory.current.path,
       helloLibraryPath,
       'libhello.dylib',
-    );
-  }
-
-  if (Platform.isWindows) {
-    libraryPath = path.join(
+    ),
+    PlatformOption.windows => path.join(
       Directory.current.path,
       helloLibraryPath,
       'Debug',
       'hello.dll',
-    );
-  }
+    ),
+    _ => throw UnsupportedError(
+      'gmat_dart does not support the Platform "${Platform.operatingSystem}".',
+    ),
+  };
 
   final dylib = ffi.DynamicLibrary.open(libraryPath);
 
