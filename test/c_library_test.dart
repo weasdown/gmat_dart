42d99254db69dc7e8bbd47fcc2f685c3fe01fc72
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-07 18:06:55 +0100

    test: add placeholder c_library_test.dart

---
+++ test/c_library_test.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:gmat_dart/src/c_library.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final Directory librariesDirectory = Directory('C_libraries');
+
+  group('CLibrary', () {
+    test('Build for hello_library', () {
+      final CLibrary helloLibrary = CLibrary(
+        sourceDirectory: librariesDirectory / 'hello_library',
+      );
+
+      // TODO add expect
+    });
+
+    // TODO implement test for running created library
+  });
+}
