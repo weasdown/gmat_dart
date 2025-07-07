7216641709d60d2117d3f2654d0cdf2a758f4dba
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-07 17:20:00 +0100

    fix: fix cmake.dart

--- lib/src/utils/cmake.dart
+++ lib/src/utils/cmake.dart
@@ -1,7 +1,7 @@
 /// @docImport: 'compile.dart';
 library;
 
-import 'dart:io' show Process;
+import 'dart:io';
 
 /// Abstract interface class for building libraries using CMake.
 ///
@@ -9,11 +9,22 @@ import 'dart:io' show Process;
 abstract interface class CMake {
   /// Build a library using CMake.
   static Future<void> build({
-    required String sourceDirectory,
-    required String buildDirectory,
+    required Directory sourceDirectory,
+    required Directory buildDirectory,
   }) async {
+    if (!sourceDirectory.isAbsolute) {
+      sourceDirectory = sourceDirectory.absolute;
+    }
+
+    if (!buildDirectory.isAbsolute) {
+      buildDirectory = buildDirectory.absolute;
+    }
+
+    // If the buildDirectory does not exist, create it.
+    buildDirectory.create();
+
     await Process.run('cmake', [
-      sourceDirectory,
-    ], workingDirectory: buildDirectory);
+      sourceDirectory.path,
+    ], workingDirectory: buildDirectory.path);
   }
 }
