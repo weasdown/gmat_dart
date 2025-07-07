ce6e83ab832d53269f930a05aaa039d3035603cc
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-07 17:20:11 +0100

    fix: fix compile.dart

--- lib/src/utils/compile.dart
+++ lib/src/utils/compile.dart
@@ -1,4 +1,5 @@
-import 'dart:io' show Platform, Process;
+import 'dart:io';
+
 import 'package:path/path.dart' as path;
 
 /// Compiles a library using the correct compiler for the platform.
@@ -9,14 +10,18 @@ Future<void> compile({
 }) async {
   // Compile library.
   if (Platform.isWindows) {
-    final String libraryName = path.basename(workingDirectory);
+    final String libraryName = path.basename(path.dirname(workingDirectory));
+
+    // If the workingDirectory does not exist, create it.
+    Directory(workingDirectory).create();
+
+    final String msbuildPath =
+        'C:\\Program Files\\Microsoft Visual Studio\\$vsYear\\$vsEdition\\MSBuild\\Current\\Bin';
 
     // Run 'msbuild' from Visual Studio.
-    await Process.run(
-      'C:\\Program Files\\Microsoft Visual Studio\\$vsYear\\$vsEdition\\MSBuild\\Current\\Bin\\MSBuild.exe',
-      ['$libraryName.sln'],
-      workingDirectory: workingDirectory,
-    );
+    await Process.run('$msbuildPath\\MSBuild.exe', [
+      '$libraryName.sln',
+    ], workingDirectory: workingDirectory);
   } else {
     // Run 'make'.
     await Process.run('make', [], workingDirectory: workingDirectory);
