8872f008596c05e513c3e4b8c07fb96ecb3628d9
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-07 17:30:21 +0100

    test: add test for utils/generate(), remove simple call of hello()

--- test/gmat_dart_test.dart
+++ test/gmat_dart_test.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
+
 import 'package:gmat_dart/gmat_dart.dart';
+import 'package:gmat_dart/hello.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    hello();
-
     setUp(() {
       // Additional setup goes here.
     });
@@ -12,6 +13,16 @@ void main() {
     test('First Test', () {
       // TODO add appropriate testing.
       // expect(awesome.isAwesome, isTrue);
+    });
+
+    test('utils/generate()', () async {
+      await generate(
+        sourceDirectory: helloLibraryRoot,
+        buildDirectory: helloLibraryBuild,
+      );
+
+      final Directory fullDirectory = Directory(helloLibraryRoot).absolute;
+      expect(await fullDirectory.exists(), true);
     });
   });
 }
