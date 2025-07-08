30f6face9d285d424028d988aff2166c0dfd8734
weasdown <34220924+weasdown@users.noreply.github.com>
2025-07-08 11:39:44 +0100

    refactor: move librariesDirectory variable to utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
 export 'src/utils/cmake.dart';
 export 'src/utils/compile.dart';
 export 'src/utils/generate.dart';
+
+final Directory librariesDirectory = Directory('C_libraries');
--- test/c_library_test.dart
+++ test/c_library_test.dart
@@ -1,12 +1,9 @@
 import 'dart:ffi' as ffi;
-import 'dart:io';
 
-import 'package:gmat_dart/src/c_library.dart';
+import 'package:gmat_dart/gmat_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final Directory librariesDirectory = Directory('C_libraries');
-
   group('CLibrary', () {
     test('Build for hello_library', () {
       final CLibrary helloLibrary = CLibrary(
